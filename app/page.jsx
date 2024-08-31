8ed8f1b7c6cad21173bf960498cae29dac407c4f
dineshvoithu <dineshkumarvoithu@gmail.com>
2024-08-31 17:17:04 +0530

    commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,25 +1,21 @@
-import Feed from "@components/Feed"
+import Feed from "@components/Feed";
 const Home = () => {
-    return (
-        <section className="w-full flex-center flex-col">
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Dicover & Sha
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
 
-            <h1 className="head_text text-center">
-                Dicover & Share
+      <p className="desc text-center">
+        Promptopia is an open source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
 
-                <br className="max-md:hidden" />
-                
-                <span className="orange_gradient text-center">AI-Powered Prompts</span>
-            </h1>
+      <Feed />
+    </section>
+  );
+};
 
-            <p className="desc text-center">
-                Promptopia is an open source AI prompting tool for modern world to discover, create and share creative prompts
-            </p>
-            
-            <Feed />
-
-      </section>
-      
-    )
-  }
-  
-  export default Home
+export default Home;
