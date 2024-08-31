35cb94858c5e0e6ed5f4970a1bb56dd1394585f6
dineshvoithu <dineshkumarvoithu@gmail.com>
2024-08-31 17:19:37 +0530

    commit

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
-        Dicover & Sha
+        Dicover & Share
         <br className="max-md:hidden" />
         <span className="orange_gradient text-center">AI-Powered Prompts</span>
       </h1>
